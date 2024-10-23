5cc3c570649994060e3edecc68241b6413a61a48
Aakash Kumar Detne <aakash.detne@yahoo.com>
2024-10-23 03:17:40 +0000

    commit

--- my_flutter_web_app/lib/main.dart
+++ my_flutter_web_app/lib/main.dart
@@ -26,7 +26,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Aakash Kumar'),
+        title: const Text('Hello'),
       ),
       body: SingleChildScrollView(
         child: Column(
